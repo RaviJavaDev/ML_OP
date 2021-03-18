1399fa6f8435fbca04f72e862ce9bc26ebfbc2ae
ravis <ravisonawane20@gmai.com>
2021-03-18 13:45:10 +0530

    'last'

--- src/split_data.py
+++ src/split_data.py
@@ -9,8 +9,8 @@ from get_data import read_params
 
 def split_and_save_data(config_path):
     config = read_params(config_path)
-    test_data_path = config['split_data']['train_path']
-    train_data_path = config['split_data']['test_path']
+    test_data_path = config['split_data']['test_path']
+    train_data_path = config['split_data']['train_path']
     raw_data_path = config['load_data']['raw_dataset_csv']
     split_ratio = config['split_data']['test_size']
     random_state = config['base']['random_state']
